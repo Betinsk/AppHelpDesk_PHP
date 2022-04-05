59691a50108bdf90bcb410d657399412b12de37e
Betinsk <gmdust@hotmail.com>
2022-04-05 11:57:14 -0300

    Autentificação

--- valida_login.php
+++ valida_login.php
@@ -1,5 +1,41 @@
 <?php 
+
+
+	//variavel que verifica se a autenticacao foi realizada
+	$usuario_autenticado = false;
+
+	//Usuarios do sistema
+	$usuarios_app = array(
+		array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+		array('email' => 'user@teste.com.br', 'senha' => 'abdc')
+	);
+
+
+
 /*
+	echo '<pre>';
+	print_r($usuarios_app);
+	echo '</pre>';
+
+*/
+
+	foreach ($usuarios_app as $user) {
+		if( $user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha'] ) {
+			$usuario_autenticado = true;
+		}
+	}
+
+	if ($usuario_autenticado == true) {
+		echo 'Usuario autenticado!';
+	}
+
+	else {
+		header('Location: index.php?login=erro' ); // força o direcionamento para a pagina
+	}
+
+
+/*
+
 	print_r($_GET);
 
 	echo "<br />";
@@ -8,7 +44,7 @@
 	echo "<br />";
 		echo $_GET['senha'];
 
-*/
+
 
 		print_r($_POST);
 
@@ -17,6 +53,6 @@
 		echo $_POST['email'];
 	echo "<br />";
 		echo $_POST['senha'];
-
+*/
 
  ?>
